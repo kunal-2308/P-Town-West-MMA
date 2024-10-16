880e271f160e0e5b9e4d2940db14ecff81d48b85
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-10-16 22:39:02 +0530

    fix: cookie issue

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,21 +2,27 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 export const protectUser = async (req, res, next) => {
-  let token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
 
   if (!token) return res.status(401).json({ message: "Not authorized" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
+
+    if (!req.user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     next();
   } catch (error) {
-    res.status(401).json({ message: "Token failed" });
+    res.status(401).json({ message: "Token failed", error: error.message });
   }
 };
 
+// Protect Admin Middleware
 export const protectAdmin = async (req, res, next) => {
-  let token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
 
   if (!token) return res.status(401).json({ message: "Not authorized" });
 
@@ -24,12 +30,13 @@ export const protectAdmin = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id);
 
+    if (!user) return res.status(404).json({ message: "User not found" });
     if (user.role !== "admin")
       return res.status(403).json({ message: "Admin access only" });
 
     req.user = user;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Token failed" });
+    res.status(401).json({ message: "Token failed", error: error.message });
   }
 };
