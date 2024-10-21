b854c4644d7c4b6928c8807d9b8699ee32f656d1
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-10-21 22:27:20 +0530

    feat: fetch previous classes

--- frontend/src/components/Admin/AdminPrevious.jsx
+++ frontend/src/components/Admin/AdminPrevious.jsx
@@ -1,7 +1,75 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const AdminPrevious = () => {
+  const [previousArray, setPreviousArray] = useState([]);
+
+  // Fetch data when component mounts
+  useEffect(() => {
+    const getPreviousClasses = async () => {
+      try {
+        let res = await axios.get(
+          `http://localhost:5007/api/classes/admin/previous`,
+          {
+            withCredentials: true,
+          }
+        );
+        setPreviousArray(res.data.previousClasses);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPreviousClasses();
+  }, []);
+
   return (
-    <div>
-      <p>Previous Class details will be shown here.</p>
+    <div className="border-2 bg-black rounded-3xl p-5">
+      <h2 className="text-2xl font-semibold mb-4 text-customYellow pl-2">
+        Previous Classes
+      </h2>
+      <div className="div-container-main grid grid-cols-3 gap-4">
+        {previousArray.slice(0, 3).map((classItem) => (
+          <div
+            className="div-card-container bg-customDark text-white rounded-3xl h-auto w-auto p-5"
+            key={classItem._id}
+          >
+            <h3 className="text-lg font-bold text-customYellow">
+              {classItem.name}
+            </h3>
+            <div className="div-content flex flex-col justify-start items-start mt-2">
+              <div className="div-content-item flex flex-row gap-2">
+                <span className="text-sm text-white">Date:</span>{" "}
+                <span className="text-sm text-white/80">
+                  {new Date(classItem.date).toLocaleDateString()}
+                </span>
+              </div>
+              <div className="div-content-item flex flex-row gap-2">
+                <span className="text-sm text-white">Time:</span>{" "}
+                <span className="text-sm text-white/80">
+                  {classItem.timeIn} - {classItem.timeOut}
+                </span>
+              </div>
+              <div className="div-content-item flex flex-row gap-2">
+                <span className="text-sm text-white">Slots:</span>{" "}
+                <span className="text-sm text-white/80">{classItem.slots}</span>
+              </div>
+              <div className="div-content-item flex flex-row gap-2">
+                <span className="text-sm text-white">Booked:</span>{" "}
+                <span className="text-sm text-white/80">
+                  {classItem.applicants.length}
+                </span>
+              </div>
+              <button
+                className="text-sm bg-customYellow p-2 rounded-lg hover:font-semibold mt-5 text-black"
+                onClick={() => handleEditClick(classItem)}
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
