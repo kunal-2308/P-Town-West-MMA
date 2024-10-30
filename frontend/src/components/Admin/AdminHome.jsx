86d829a7051125f2e4cddc4cf3406717b6e75834
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-10-30 11:31:16 +0530

    fix: responsiveness

--- frontend/src/components/Admin/AdminHome.jsx
+++ frontend/src/components/Admin/AdminHome.jsx
@@ -10,7 +10,7 @@ const AdminHome = ({ onViewAllClick }) => {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isViewModalOpen, setIsViewModalOpen] = useState(false);
   const [selectedClassId, setSelectedClassId] = useState(null);
-  const [viewClassItem, setViewClassItem] = useState(null); // Store class item for viewing details
+  const [viewClassItem, setViewClassItem] = useState(null);
   const [categories, setCategories] = useState([]);
   const [instructors, setInstructors] = useState([]);
 
@@ -45,7 +45,7 @@ const AdminHome = ({ onViewAllClick }) => {
         setCategories(res.data.categories);
         setInstructors(res.data.instructors);
       } catch (error) {
-        console.error('Error fetching categories or instructors', error);
+        console.error("Error fetching categories or instructors", error);
       }
     };
 
@@ -54,11 +54,10 @@ const AdminHome = ({ onViewAllClick }) => {
     fetchCategoriesAndInstructors();
   }, []);
 
-  // Function to convert 24-hour format to 12-hour format with AM/PM
   const formatTimeTo12Hour = (time) => {
     const [hours, minutes] = time.split(":");
     const period = hours >= 12 ? "PM" : "AM";
-    const adjustedHours = hours % 12 || 12; // Convert '0' or '12' to '12' for 12-hour format
+    const adjustedHours = hours % 12 || 12;
     return `${adjustedHours}:${minutes} ${period}`;
   };
 
@@ -72,55 +71,73 @@ const AdminHome = ({ onViewAllClick }) => {
   };
 
   const handleViewDetails = (classItem) => {
-    setViewClassItem(classItem); // Store the entire class object
+    setViewClassItem(classItem);
     setIsViewModalOpen(true);
   };
 
   return (
-    <div className="space-y-3">
+    <div className="space-y-3 p-4 sm:p-6 lg:p-8">
       {/* Upcoming Classes */}
       <div className="border-2 bg-black rounded-3xl p-5">
-        <h2 className="text-2xl font-semibold mb-4 text-customYellow pl-2">Upcoming Classes</h2>
-        <div className="grid grid-cols-3 gap-4">
+        <h2 className="text-2xl font-semibold mb-4 text-customYellow pl-2">
+          Upcoming Classes
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {upcomingArray.slice(0, 3).map((classItem) => (
-            <div className="bg-customDark text-white rounded-3xl p-5" key={classItem._id}>
-              <h3 className="text-lg font-bold text-customYellow">{classItem.name}</h3>
-              <div className="flex flex-col mt-2">
+            <div
+              className="bg-customDark text-white rounded-3xl p-5"
+              key={classItem._id}
+            >
+              <h3 className="text-lg font-bold text-customYellow">
+                {classItem.name}
+              </h3>
+              <div className="flex flex-col mt-2 space-y-2">
                 <div className="flex flex-row gap-2">
                   <span className="text-sm text-white">Date:</span>
-                  <span className="text-sm text-white/80">{new Date(classItem.date).toLocaleDateString()}</span>
+                  <span className="text-sm text-white/80">
+                    {new Date(classItem.date).toLocaleDateString()}
+                  </span>
                 </div>
                 <div className="flex flex-row gap-2">
                   <span className="text-sm text-white">Time:</span>
                   <span className="text-sm text-white/80">
-                    {formatTimeTo12Hour(classItem.timeIn)} - {formatTimeTo12Hour(classItem.timeOut)}
+                    {formatTimeTo12Hour(classItem.timeIn)} -{" "}
+                    {formatTimeTo12Hour(classItem.timeOut)}
                   </span>
                 </div>
                 <div className="flex flex-row gap-2">
                   <span className="text-sm text-white">Slots:</span>
-                  <span className="text-sm text-white/80">{classItem.slots}</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.slots}
+                  </span>
                 </div>
                 <div className="flex flex-row gap-2">
                   <span className="text-sm text-white">Booked:</span>
-                  <span className="text-sm text-white/80">{classItem.applicants.length}</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.applicants.length}
+                  </span>
                 </div>
                 <div className="flex flex-row gap-2">
                   <span className="text-sm text-white">Instructor:</span>
-                  <span className="text-sm text-white/80">{classItem.instructor}</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.instructor}
+                  </span>
                 </div>
                 <div className="flex flex-row gap-2">
                   <span className="text-sm text-white">Category:</span>
-                  <span className="text-sm text-white/80">{classItem.category}</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.category}
+                  </span>
                 </div>
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-2 mt-5">
                   <button
-                    className="text-sm bg-customYellow p-2 rounded-lg mt-5 text-black"
+                    className="text-sm bg-customYellow px-3 py-2 rounded-lg text-black w-full"
                     onClick={() => handleEditClick(classItem._id)}
                   >
                     Edit Event
                   </button>
                   <button
-                    className="text-sm bg-customYellow p-2 rounded-lg mt-5 text-black"
+                    className="text-sm bg-customYellow px-3 py-2 rounded-lg text-black w-full"
                     onClick={() => handleViewDetails(classItem)}
                   >
                     View Details
@@ -133,52 +150,66 @@ const AdminHome = ({ onViewAllClick }) => {
       </div>
 
       {/* Previous Classes */}
-      <div className="space-y-3">
-        <div className="border-2 bg-black rounded-3xl p-5">
-          <h2 className="text-2xl font-semibold mb-4 text-customYellow pl-2">Previous Classes</h2>
-          <div className="grid grid-cols-3 gap-4">
-            {previousArray.slice(0, 3).map((classItem) => (
-              <div className="bg-customDark text-white rounded-3xl p-5" key={classItem._id}>
-                <h3 className="text-lg font-bold text-customYellow">{classItem.name}</h3>
-                <div className="flex flex-col mt-2">
-                  <div className="flex flex-row gap-2">
-                    <span className="text-sm text-white">Date:</span>
-                    <span className="text-sm text-white/80">{new Date(classItem.date).toLocaleDateString()}</span>
-                  </div>
-                  <div className="flex flex-row gap-2">
-                    <span className="text-sm text-white">Time:</span>
-                    <span className="text-sm text-white/80">
-                      {formatTimeTo12Hour(classItem.timeIn)} - {formatTimeTo12Hour(classItem.timeOut)}
-                    </span>
-                  </div>
-                  <div className="flex flex-row gap-2">
-                    <span className="text-sm text-white">Slots:</span>
-                    <span className="text-sm text-white/80">{classItem.slots}</span>
-                  </div>
-                  <div className="flex flex-row gap-2">
-                    <span className="text-sm text-white">Booked:</span>
-                    <span className="text-sm text-white/80">{classItem.applicants.length}</span>
-                  </div>
-                  <div className="flex flex-row gap-2">
-                    <span className="text-sm text-white">Instructor:</span>
-                    <span className="text-sm text-white/80">{classItem.instructor}</span>
-                  </div>
-                  <div className="flex flex-row gap-2">
-                    <span className="text-sm text-white">Category:</span>
-                    <span className="text-sm text-white/80">{classItem.category}</span>
-                  </div>
-                  <div className="flex flex-row gap-2">
-                    <button
-                      className="text-sm bg-customYellow p-2 rounded-lg mt-5 text-black"
-                      onClick={() => handleViewDetails(classItem)}
-                    >
-                      View Details
-                    </button>
-                  </div>
+      <div className="border-2 bg-black rounded-3xl p-5">
+        <h2 className="text-2xl font-semibold mb-4 text-customYellow pl-2">
+          Previous Classes
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {previousArray.slice(0, 3).map((classItem) => (
+            <div
+              className="bg-customDark text-white rounded-3xl p-5"
+              key={classItem._id}
+            >
+              <h3 className="text-lg font-bold text-customYellow">
+                {classItem.name}
+              </h3>
+              <div className="flex flex-col mt-2 space-y-2">
+                <div className="flex flex-row gap-2">
+                  <span className="text-sm text-white">Date:</span>
+                  <span className="text-sm text-white/80">
+                    {new Date(classItem.date).toLocaleDateString()}
+                  </span>
                 </div>
+                <div className="flex flex-row gap-2">
+                  <span className="text-sm text-white">Time:</span>
+                  <span className="text-sm text-white/80">
+                    {formatTimeTo12Hour(classItem.timeIn)} -{" "}
+                    {formatTimeTo12Hour(classItem.timeOut)}
+                  </span>
+                </div>
+                <div className="flex flex-row gap-2">
+                  <span className="text-sm text-white">Slots:</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.slots}
+                  </span>
+                </div>
+                <div className="flex flex-row gap-2">
+                  <span className="text-sm text-white">Booked:</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.applicants.length}
+                  </span>
+                </div>
+                <div className="flex flex-row gap-2">
+                  <span className="text-sm text-white">Instructor:</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.instructor}
+                  </span>
+                </div>
+                <div className="flex flex-row gap-2">
+                  <span className="text-sm text-white">Category:</span>
+                  <span className="text-sm text-white/80">
+                    {classItem.category}
+                  </span>
+                </div>
+                <button
+                  className="text-sm bg-customYellow px-3 py-2 rounded-lg text-black w-full mt-5"
+                  onClick={() => handleViewDetails(classItem)}
+                >
+                  View Details
+                </button>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
 
@@ -196,7 +227,7 @@ const AdminHome = ({ onViewAllClick }) => {
       <ViewClassModal
         isOpen={isViewModalOpen}
         onClose={() => setIsViewModalOpen(false)}
-        classItem={viewClassItem} // Pass the entire class object
+        classItem={viewClassItem}
       />
     </div>
   );
