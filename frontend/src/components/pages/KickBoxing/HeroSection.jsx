b5e26b8d27ab0e6a514b159937e11cecbe232e09
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-11-23 12:08:42 +0530

    fix: images

--- frontend/src/components/pages/KickBoxing/HeroSection.jsx
+++ frontend/src/components/pages/KickBoxing/HeroSection.jsx
@@ -1,6 +1,7 @@
 import { Button } from "../../ui/button";
 import { MoveRight } from "lucide-react";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function HeroSection() {
   return (
@@ -9,17 +10,17 @@ function HeroSection() {
         {/* Background Image */}
         <div
           className="
-          absolute top-0 left-0 w-full h-full
+          absolute top-0 left-0 w-full h-full 
           bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: `url('/images/Home/Trainings/26.png')`,
+            backgroundImage: `url('/Photos_Ptown/KickBoxing/Hero_PTown MMA.png')`,
           }}
         ></div>
 
         {/* Content Section */}
-        <div className="w-screen relative z-10 flex flex-col items-center justify-center">
+        <div className="w-screen mt-40 relative z-10 flex flex-col items-center justify-center">
           {/* Boxing/Kickboxing Text */}
-          <div className="div1 mt-32 lg:mt-28 xl:mt-[200px]">
+          <div className="div1 mt-36 lg:mt-28 xl:mt-[200px]">
             <span className=" font-semibold md:font-extrabold text-white text-2xl md:text-7xl">
               BOXING / KICK BOXING
             </span>
@@ -31,7 +32,7 @@ function HeroSection() {
             <div className="flex flex-col justify-start items-start w-full lg:w-[600px] mb-8 lg:mb-0 pl-10">
               <div className="flex justify-center items-center">
                 <span className="text-2xl sm:text-3xl md:text-4xl font-semibold text-gray-400 mr-4">
-        ////
+                  ////
                 </span>
                 <span className="text-xl sm:text-2xl text-white font-thin pt-2">
                   Master Kickboxing with Expert Training
@@ -49,23 +50,25 @@ function HeroSection() {
             {/* Right Section */}
             <div className="flex flex-col w-full lg:w-[400px] pr-10">
               <span className="text-white text-sm sm:text-base md:text-sm font-thin">
-                Discover the power and intensity of our Kickboxing Training Program at
-                P-TownWest MMA in Pune, Baner. Designed for all skill levels, our program
-                helps you master striking techniques, build endurance, and enhance your
-                overall fitness under the guidance of our expert trainers.
+                Discover the power and intensity of our Kickboxing Training
+                Program at P-TownWest MMA in Pune, Baner. Designed for all skill
+                levels, our program helps you master striking techniques, build
+                endurance, and enhance your overall fitness under the guidance
+                of our expert trainers.
               </span>
-              <Button className="bg-customYellow text-black w-[150px] flex justify-center items-center gap-x-3 rounded-full mt-5 h-[30px] hover:bg-customYellow hover:cursor-pointer">
-                Contact Us <MoveRight />
-              </Button>
+              <NavLink to="/contact">
+                <Button className="bg-customYellow text-black w-[150px] flex justify-center items-center gap-x-3 rounded-full mt-5 h-[30px] hover:bg-customYellow hover:cursor-pointer">
+                  Contact Us <MoveRight />
+                </Button>
+              </NavLink>
             </div>
           </div>
-
         </div>
       </div>
       <div className="div-mobile-view block md:hidden bg-black w-screen h-auto -mt-1 pl-5">
         <div className="div-a flex justify-start items-center pl-5">
           <span className="text-3xl md:text-4xl font-semibold text-gray-400 mr-4">
-                ////
+            ////
           </span>
           <span className="text-xs text-white font-thin pt-2">
             Master Kickboxing with Expert Training
@@ -74,17 +77,16 @@ function HeroSection() {
         <div className="div-b flex justify-start items-start mt-3 pl-4  w-screen pr-10">
           <span className=" text-customYellow text-3xl md:text-4xl lg:text-5xl font-medium">
             Kickboxing / Boxing <br />
-            Training Program in
-            Pune, Baner
+            Training Program in Pune, Baner
           </span>
         </div>
         <div className="div-2-2a flex flex-col justify-start items-start pl-4 pr-10 w-full lg:w-screen mt-4 pb-10 ">
           <span className="text-white text-xs sm:text-base md:text-sm font-thin">
-            Discover the power and intensity of our Kickboxing Training
-            Program at P-TownWest MMA in Pune, Baner. Designed for all skill
-            levels, our program helps you master striking techniques, build
-            endurance, and enhance your overall fitness under the guidance of
-            our expert trainers.
+            Discover the power and intensity of our Kickboxing Training Program
+            at P-TownWest MMA in Pune, Baner. Designed for all skill levels, our
+            program helps you master striking techniques, build endurance, and
+            enhance your overall fitness under the guidance of our expert
+            trainers.
           </span>
           <Button className="bg-customYellow text-black w-[150px] flex justify-center items-center gap-x-3 rounded-full mt-5 h-[30px] hover:bg-customYellow hover:cursor-pointer">
             Contact Us <MoveRight />
--- frontend/src/components/pages/KickBoxing/InfoSection.jsx
+++ frontend/src/components/pages/KickBoxing/InfoSection.jsx
@@ -1,45 +1,58 @@
-import React from 'react';
+import React from "react";
 
 function InfoSection() {
-    return (
-        <>
-            <div className="div flex justify-center items-center p-4 lg:p-8">
-                <div className="container flex flex-col md:flex-row justify-center items-center gap-4 md:gap-6">
-                    {/* Left Image */}
-                    <div className="div-1-image flex-shrink-0 w-full md:w-[50%]">
-                        <img 
-                            src="/images/Kickboxing/4.png" 
-                            alt="image" 
-                            className='h-[240px] sm:h-[300px] md:h-[360px] lg:h-[460px] xl:h-[455px] object-cover w-full rounded-lg' 
-                        />
-                    </div>
-                    
-                    {/* Text Sections */}
-                    <div className="div-2 flex flex-col gap-4 w-full md:w-[50%]">
-                        {/* First Content Block */}
-                        <div className="div-2-1-content bg-customYellow flex flex-col justify-center items-center w-full h-auto rounded-lg p-4 md:p-6 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105">
-                            <div className="div-a flex justify-center items-center flex-col gap-y-3">
-                                <span className='font-semibold text-lg sm:text-xl md:text-2xl text-center'>Footwork and Movement:</span>
-                                <span className='font-light text-xs sm:text-sm md:text-base mt-3 px-4 text-center'>
-                                    Effective movement and footwork are crucial in striking sports. Participants will receive specialized instruction on how to move efficiently, maintain balance, and control distance, enabling them to engage opponents effectively in both boxing and kickboxing scenarios.
-                                </span>
-                            </div>
-                        </div>
+  return (
+    <>
+      <div className="div flex justify-center items-center p-4 lg:p-8">
+        <div className="container flex flex-col md:flex-row justify-center items-center gap-4 md:gap-6">
+          {/* Left Image */}
+          <div className="div-1-image flex-shrink-0 w-full md:w-[50%]">
+            <img
+              src="/Photos_Ptown/KickBoxing/2.Card_PTown MMA.png"
+              alt="image"
+              className="h-[240px] sm:h-[300px] md:h-[360px] lg:h-[460px] xl:h-[455px] object-cover w-full rounded-lg"
+            />
+          </div>
 
-                        {/* Second Content Block */}
-                        <div className="div-2-1-content bg-black text-white flex flex-col justify-center items-center w-full h-auto rounded-lg p-4 md:p-6 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105">
-                            <div className="div-a flex justify-center items-center flex-col gap-y-3">
-                                <span className='font-semibold text-lg sm:text-xl md:text-2xl text-center'>Conditioning and Fitness:</span>
-                                <span className='font-light text-xs sm:text-sm md:text-base mt-3 px-4 text-center'>
-                                    Striking requires exceptional cardiovascular endurance and overall fitness. Through targeted workouts, participants will improve their stamina, strength, and agility, preparing them for the physical demands of both sports. Conditioning drills, circuit training, and core exercises will enhance their performance and resilience.
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          {/* Text Sections */}
+          <div className="div-2 flex flex-col gap-4 w-full md:w-[50%]">
+            {/* First Content Block */}
+            <div className="div-2-1-content bg-customYellow flex flex-col justify-center items-center w-full h-auto rounded-lg p-4 md:p-6 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105">
+              <div className="div-a flex justify-center items-center flex-col gap-y-3">
+                <span className="font-semibold text-lg sm:text-xl md:text-2xl text-center">
+                  Footwork and Movement:
+                </span>
+                <span className="font-light text-xs sm:text-sm md:text-base mt-3 px-4 text-center">
+                  Effective movement and footwork are crucial in striking
+                  sports. Participants will receive specialized instruction on
+                  how to move efficiently, maintain balance, and control
+                  distance, enabling them to engage opponents effectively in
+                  both boxing and kickboxing scenarios.
+                </span>
+              </div>
             </div>
-        </>
-    );
+
+            {/* Second Content Block */}
+            <div className="div-2-1-content bg-black text-white flex flex-col justify-center items-center w-full h-auto rounded-lg p-4 md:p-6 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105">
+              <div className="div-a flex justify-center items-center flex-col gap-y-3">
+                <span className="font-semibold text-lg sm:text-xl md:text-2xl text-center">
+                  Conditioning and Fitness:
+                </span>
+                <span className="font-light text-xs sm:text-sm md:text-base mt-3 px-4 text-center">
+                  Striking requires exceptional cardiovascular endurance and
+                  overall fitness. Through targeted workouts, participants will
+                  improve their stamina, strength, and agility, preparing them
+                  for the physical demands of both sports. Conditioning drills,
+                  circuit training, and core exercises will enhance their
+                  performance and resilience.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default InfoSection;
