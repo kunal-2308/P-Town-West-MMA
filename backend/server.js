d6620d75676d43bb8458949d457d9061625f8a79
kunal-2308 <kunaltaware210@gmail.com>
2024-12-20 00:54:44 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ptownmma.vercel.app/",
     credentials: true,
   })
 );
--- frontend/configure.js
+++ frontend/configure.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:5007";
+// export const API_URL = "http://localhost:5007";
+export const API_URL = "https://p-town-west-mma-1.onrender.com";
