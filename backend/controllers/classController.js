79d123fa91552882fd01843e1233ebb97373446f
kunal-2308 <kunaltaware210@gmail.com>
2024-12-21 20:07:35 +0530

    .

--- backend/controllers/classController.js
+++ backend/controllers/classController.js
@@ -104,7 +104,7 @@ export const getBookedClasses = async (req, res) => {
   const userId = req.user.id;
 
   try {
-    const user = await User.findById(userId).populate("bookedClasses");
+    const user = await User.findById(userId).populate("bookedClasses").sort({date:1});
     if (!user) return res.status(404).json({ message: "User not found" });
 
     res.status(200).json(user.bookedClasses);
