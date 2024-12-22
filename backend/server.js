9331e9e211bf86c961ea2e5d73d8fcdfc039e66f
kunal-2308 <kunaltaware210@gmail.com>
2024-12-22 15:42:52 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    // origin: "https://ptownmma.vercel.app",
-    origin:"http://localhost:5173"
+    origin: "https://ptownmma.vercel.app",
+    // origin:"http://localhost:5173"
     // origin:"*"
   })
 );
