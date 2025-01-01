9f51710f0a42c7e4c6b799f02ea59bf40e5ddf8b
krishna100k <krishnaprasadawala@gmail.com>
2025-01-01 18:17:21 +0530

    api url changed

--- frontend/configure.js
+++ frontend/configure.js
@@ -2,4 +2,5 @@
 // export const API_URL = "https://p-town-west-mma-1.onrender.com";
 
 //Production
-export const API_URL = "http://165.22.219.234";
+// export const API_URL = "http://165.22.219.234";
+export const API_URL = "https://ptownwestmma.in";
