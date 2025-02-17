5a6111e92a3e66f2a7bbd6e079e11d8ccf740b96
Krishnaprasad Awala <102644745+krishna100k@users.noreply.github.com>
2025-02-18 01:50:24 +0530

    Update configure.js

--- frontend/configure.js
+++ frontend/configure.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://localhost:1337";
-// export const API_URL = "https://ptownmma.onrender.com";
+export const API_URL = "https://ptownmma.onrender.com";
 
 //Production
 // export const API_URL = "http://165.22.219.234";
-export const API_URL = "https://ptownwestmma.in";
+// export const API_URL = "https://ptownwestmma.in";
